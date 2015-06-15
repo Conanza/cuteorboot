5753a8489654c322775f3b4fd11fc659f7c65d0b
Conan Tzou <conantzou@gmail.com>
2015-06-15 13:26:11 -0700

    refactor js code from templates into views

--- app/assets/javascripts/views/top_cuties/top_cutie_item.js
+++ app/assets/javascripts/views/top_cuties/top_cutie_item.js
@@ -8,10 +8,32 @@ CuteOrBoot.Views.TopCutie = Backbone.CompositeView.extend({
   },
 
   render: function () {
-    var content = this.template({ user: this.model });
+    var content = this.template({
+      user: this.model,
+      ratingClass: this.ratingClass()
+    });
     this.$el.html(content);
     this.$("img").attr("cutie-id", this.model.id);
 
     return this;
+  },
+
+  ratingClass: function () {
+    var rating = this.model.escape("rating");
+
+    var ratingClass;
+    if (rating > 9) {
+      ratingClass = "rating-sizzling";
+    } else if (rating > 7) {
+      ratingClass = "rating-hot";
+    } else if (rating > 6) {
+      ratingClass = "rating-warm";
+    } else if (rating >= 5){
+      ratingClass = "rating-lukewarm";
+    } else {
+      ratingClass = "rating-cold";
+    }
+
+    return ratingClass;
   }
 });
