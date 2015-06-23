7fc3f4b41415a316707bc6a52f3669276bc1fad8
Conan Tzou <conantzou@gmail.com>
2015-06-22 17:49:27 -0700

    edit tour wording

--- app/assets/javascripts/cob_tour.js
+++ app/assets/javascripts/cob_tour.js
@@ -14,7 +14,7 @@ window.CuteOrBootTour = {
         template: function () {
           var popover = "<div class='popover tour'><h3 class='popover-title'></h3><div class='popover-content'></div><div class='popover-navigation'><div class='btn-group'><button class='btn btn-default' data-role='next'>Next »</button></div><button class='btn btn-default' data-role='end'>End tour</button></div></div>";
           return popover;
-        }
+        },
       },
       {
         element: ".game.col-xs-12",
@@ -86,7 +86,7 @@ window.CuteOrBootTour = {
         element: ".showFans.col-xs-12",
         placement: "right",
         title: "Your Pet's Fans",
-        content: "You can also browse through all the users who've voted your pet cute!",
+        content: "You can also browse through all the pets who've voted your pet cute!",
         onNext: function (tour) {
           document.location.href = "/#search";
         }
@@ -95,16 +95,20 @@ window.CuteOrBootTour = {
         element: ".searchUsers.col-xs-12",
         placement: "right",
         title: "Pet Search",
-        content: "Check this out to search for pets by name, state, and more!",
+        content: "Check this out to search for pets by name, state, and more.",
         onNext: function (tour) {
           document.location.href = "/#petreel";
         }
       },
       {
-        element: "body",
+        orphan: true,
         placement: "top",
-        title: "Thanks!",
-        content: "Alright. I'll let you get to it."
+        title: "Have fun!",
+        content: "Thanks again for joining Cute Or Boot. I'll let you get to voting. Don't forget to add pictures of your pet!",
+        template: function () {
+          var popover = "<div class='popover tour'><h3 class='popover-title'></h3><div class='popover-content'></div><div class='popover-navigation'><button class='btn btn-default' data-role='end'>Yay!</button></div></div>";
+          return popover;
+        }
       }
     ]);
 
