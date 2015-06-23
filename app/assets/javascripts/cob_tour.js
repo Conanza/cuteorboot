31073159628ede97a903194c7a21bb4f25bd6eda
Conan Tzou <conantzou@gmail.com>
2015-06-22 17:35:44 -0700

    finish tour

--- app/assets/javascripts/cob_tour.js
+++ app/assets/javascripts/cob_tour.js
@@ -9,10 +9,10 @@ window.CuteOrBootTour = {
         element: ".cuteorboot",
         placement: "top",
         backdrop: true,
-        title: "Welcome to Cute or Boot!",
+        title: "Welcome to Cute Or Boot!",
         content: "Glad you decided to join us! You're in for a treat.<br><br>If you've ever used HotOrNot, this should be very familiar. If not, I won't keep you away from the super cute pets for long. Just going to give you a quick guide through the app's features!",
         template: function () {
-          var popover = "<div class='popover tour'><h3 class='popover-title'></h3><div class='popover-content'></div><div class='popover-navigation'><button class='btn btn-default' data-role='prev'>« Prev</button><button class='btn btn-default' data-role='next'>Next »</button><button class='btn btn-default' data-role='end'>End tour</button></div></div>";
+          var popover = "<div class='popover tour'><h3 class='popover-title'></h3><div class='popover-content'></div><div class='popover-navigation'><div class='btn-group'><button class='btn btn-default' data-role='next'>Next »</button></div><button class='btn btn-default' data-role='end'>End tour</button></div></div>";
           return popover;
         }
       },
@@ -47,12 +47,65 @@ window.CuteOrBootTour = {
         placement: "left",
         title: "Who's next??",
         content: "See who's up next!",
-        onNext: function () {
-          document.location.href = '/#cuties/' + CURRENT_USER_ID;
-          CuteOrBootTourDeux.initialize();
+        onNext: function (tour) {
+          document.location.href = "/#cuties/" + CURRENT_USER_ID;
         }
       },
-      {}
+      {
+        element: "li.col-xs-12.showuser",
+        placement: "right",
+        title: "Your Pet's Profile",
+        content: "Viewing your pet's profile offers a few more options.<br><br>Hover over this to logout.",
+        reflex: true
+      },
+      {
+        element: "li.tour-upload-step",
+        placement: "bottom",
+        title: "Upload Images",
+        content: "Add more pictures here to get more cute votes!"
+      },
+      {
+        element: "li.tour-editprofile-step",
+        placement: "bottom",
+        title: "Edit Profile",
+        content: "You can change your pet's information here.",
+        onNext: function (tour) {
+          document.location.href = "/#top-cuties-list";
+        }
+      },
+      {
+        element: ".topCutiesList.col-xs-12",
+        placement: "right",
+        title: "The Cutest Cuties",
+        content: "Almost done!<br><br>Make sure to check out Cute Or Boot's highest rated pets.",
+        onNext: function (tour) {
+          document.location.href = "/#fans";
+        }
+      },
+      {
+        element: ".showFans.col-xs-12",
+        placement: "right",
+        title: "Your Pet's Fans",
+        content: "You can also browse through all the users who've voted your pet cute!",
+        onNext: function (tour) {
+          document.location.href = "/#search";
+        }
+      },
+      {
+        element: ".searchUsers.col-xs-12",
+        placement: "right",
+        title: "Pet Search",
+        content: "Check this out to search for pets by name, state, and more!",
+        onNext: function (tour) {
+          document.location.href = "/#petreel";
+        }
+      },
+      {
+        element: "body",
+        placement: "top",
+        title: "Thanks!",
+        content: "Alright. I'll let you get to it."
+      }
     ]);
 
     tour.init();
