aa609b90c5873e1ceedef397fac08ab9a1ea0ab6
Conan Tzou <conantzou@gmail.com>
2015-07-08 10:35:33 -0700

    begin revamp of home page

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -19,6 +19,16 @@
 //= require backbone
 //= require users_search
 //= require jquery.slick
+//= require modernizr.custom
+//= require jquery.parallax-1.1.3
+//= require imagesloaded.pkgd.js
+//= require jquery.sticky
+//= require smoothscroll
+//= require wow.min
+//= require jquery.easypiechart
+//= require waypoints.min
+//= require jquery.cbpQTRotator
+//= require custom
 //= require cute_or_boot
 //= require_tree ../templates
 //= require_tree ./utils
